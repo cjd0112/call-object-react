004a9d1b6a43d64cdb419a09b8b03c610ad2676b
cjd0112 <colin.dick@alphastorm.co.uk>
2021-10-23 21:18:35 +0100

    foo

--- src/components/Call/callState.js
+++ src/components/Call/callState.js
@@ -138,7 +138,7 @@ function getMessage(callState) {
   } else if (shouldShowClickAllow()) {
     header = 'Click "Allow" to enable camera and mic access';
   } else if (Object.keys(callState.callItems).length === 1) {
-    header = "Copy and share this page's URL to invite others";
+    header = "Copy and share this page's URL to invite others - foo";
     detail = window.location.href;
   }
   return header || detail ? { header, detail, isError } : null;
